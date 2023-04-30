193923eccecd20e924c7e26d1f1ad5453dca1d5d
Simmone Kelly <simmone.kelly@gmail.com>
2023-04-30 18:29:14 -0400

    moving card selection to player view

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,15 @@
 import "./App.css";
-import { io, Socket } from "socket.io-client";
-import React from "react";
-import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
 import SkullHero from "./images/SkullHero.jpg";
-// import { RoseCard } from "./components/RoseCard";
 import { PlayerView } from "./pages/PlayerView";
 const socket = io("http://localhost:8080");
 
 function App() {
-  const [topCard, setTopCard] = useState(Boolean);
-  const [pickedCards, setPickedCards] = useState(Array);
-
-  //opponents cards
-  const [oppTopCard, setOppTopCard] = useState("");
-  const [oppPickedCards, setOppPickedCards] = useState([]);
-
   //creating a new room
   const createNewRoom = () => {
     console.log("create new room");
     socket.emit("create_room", { room: "new room name" });
   };
-
-  //sending message to backend
-  const confirmDeck = () => {
-    console.log("card picked");
-    socket.emit("card_picked", { topCard, pickedCards });
-  };
-
-  const setCard = (id: number, isRose: boolean) => {
-    setTopCard(isRose);
-    setPickedCards([...pickedCards, id]);
-  };
-
-  useEffect(() => {
-    socket.on("display_picked_card", (data: any) => {
-      setOppTopCard(data.topCard);
-      setOppPickedCards(data.pickedCards);
-    });
-  }); //, [socket]);
-
-  console.log({ topCard, pickedCards, oppTopCard, oppPickedCards });
 
   return (
     <div className="App">
@@ -51,21 +21,6 @@ function App() {
       <div>
         <input placeholder="Input Room Number To Join" />
         <button>Join Room</button>
-      </div>
-
-      <div>Game Room With Up to 6 People</div>
-      <div>
-        <div onClick={() => setCard(1, false)}>rose</div>
-        <div onClick={() => setCard(2, false)}>rose</div>
-        <div onClick={() => setCard(3, true)}>skull</div>
-        <div onClick={() => setCard(4, false)}>rose</div>
-      </div>
-      <button onClick={confirmDeck}>Submit</button>
-      <div>
-        Opponents Cards
-        <div>Top Card: {oppTopCard ? "skull" : "rose"}</div>
-        <div>Picked Cards: {oppPickedCards}</div>
-        <div>Cards Left: {4 - oppPickedCards.length}</div>
       </div>
       <PlayerView />
     </div>
--- src/pages/PlayerView.tsx
+++ src/pages/PlayerView.tsx
@@ -1,24 +1,63 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { RoseCard } from "../components/RoseCard";
+import { io } from "socket.io-client";
+const socket = io("http://localhost:8080");
 
 type Props = {
   room?: number;
 };
 
 export const PlayerView: React.FC<Props> = ({ room }) => {
+  //current players cards
   const [topCard, setTopCard] = useState(Boolean);
   const [pickedCards, setPickedCards] = useState(Array);
 
+  //opponents cards
+  const [oppTopCard, setOppTopCard] = useState("");
+  const [oppPickedCards, setOppPickedCards] = useState([]);
+
+  //sending selected card to backend
+  const submitCard = () => {
+    console.log("card picked");
+    socket.emit("card_picked", { topCard, pickedCards });
+  };
+
+  //setting the selected card
   const setCard = (id: number, isRose: boolean) => {
     setTopCard(isRose);
     setPickedCards([...pickedCards, id]);
   };
 
+  //recieving data from back end of opponents card selection
+  useEffect(() => {
+    socket.on("display_picked_card", (data: any) => {
+      console.log({ data });
+      setOppTopCard(data.topCard);
+      setOppPickedCards(data.pickedCards);
+    });
+  }); //, [socket]);
+
+  console.log({ topCard, pickedCards, oppTopCard, oppPickedCards });
+
   return (
     <div>
       <h1>Player View</h1>
+      <div>
+        Opponents Cards
+        <div>Top Card: {oppTopCard ? "skull" : "rose"}</div>
+        <div>Picked Cards: {oppPickedCards.length}</div>
+        <div>Cards Left: {4 - oppPickedCards.length}</div>
+      </div>
+      <hr />
       <RoseCard />
+      <div>
+        <div onClick={() => setCard(1, false)}>rose</div>
+        <div onClick={() => setCard(2, false)}>rose</div>
+        <div onClick={() => setCard(3, true)}>skull</div>
+        <div onClick={() => setCard(4, false)}>rose</div>
+      </div>
+      <button onClick={submitCard}>Submit</button>
     </div>
   );
 };
