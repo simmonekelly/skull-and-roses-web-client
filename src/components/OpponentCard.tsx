33ae8f3a8a680858039594af066adef105c12b0d
Simmone Kelly <simmone.kelly@onepeloton.com>
2024-09-11 17:09:40 -0400

    adding styling to opps section

--- src/components/OpponentCard.tsx
+++ src/components/OpponentCard.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { styled } from "styled-components";
+import LocalPlayRoundedIcon from "@mui/icons-material/LocalPlayRounded";
 
 export const OpponentCard: React.FC = () => {
   return (
     <Container>
-      <p>Hidden Card </p>
+      <LocalPlayRoundedIcon />
     </Container>
   );
 };
 
 const Container = styled.div`
-  margin: 1rem;
+  padding: 10px;
+  margin: 10px;
+  display: flex;
+  border: 1px solid black;
+  border-radius: 4px;
 `;
